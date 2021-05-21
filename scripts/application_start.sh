a1d84f140f7183d432e76a659514ccf5b0b82eb9
Jason Pham <61170796+jasonhtpham@users.noreply.github.com>
2021-05-21 16:32:27 +1000

    Add scripts + yml for ci/cd

---
+++ scripts/application_start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#give permission for everything in the express-app directory
+sudo chmod -R 777 /home/storyar-user/StoryAR-backend
+
+#navigate into our working directory where we have all our github files
+cd /home/storyar-user/StoryAR-backend
+
+#add npm and node to path
+export NVM_DIR="$HOME/.nvm"	
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
+
+#install node modules
+npm install
+
+#build package
+npm build
+
+#run deployment
+npm run deployment
